a99527f2a3df53e76a77dc89af647af86b3907e7
seyiakins <seyibass10@gmail.com>
2025-11-06 10:22:43 +0100

    Update AdminDashboard with full tab components and AddProperty integration

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -6,6 +6,7 @@ import DashboardCard from "../components/DashboardCard";
 import AnalyticsChart from "../components/AnalyticsChart";
 import UserManagement from "../components/UserManagement";
 import InquiriesMessages from "../components/InquiriesMessages";
+import AddProperty from "../pages/AddProperty"; // <-- Import your AddProperty page
 
 const AdminDashboard = () => {
   const [activeTab, setActiveTab] = useState("dashboard");
@@ -75,6 +76,7 @@ const AdminDashboard = () => {
       <div className="flex-1 flex flex-col overflow-auto md:ml-64">
         <TopBar />
 
+        {/* Dashboard Overview */}
         {activeTab === "dashboard" && (
           <div className="p-6">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
@@ -95,12 +97,13 @@ const AdminDashboard = () => {
           </div>
         )}
 
-        {activeTab === "add-property" && (
-          <div className="p-6">Add Property Form will go here</div>
-        )}
+        {/* Add Property */}
+        {activeTab === "add-property" && <AddProperty />}
 
+        {/* User Management */}
         {activeTab === "user-management" && <UserManagement users={users} />}
 
+        {/* Inquiries & Messages */}
         {activeTab === "inquiries" && <InquiriesMessages inquiries={inquiries} />}
       </div>
     </div>
